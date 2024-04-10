84af83df259f03f1814ec4a6f40487000382d4b0
Andrew <15205112+andrews4s@users.noreply.github.com>
2024-04-10 17:57:29 -0400

    Fix layout issues when compiling for modern .net versions.

--- src/CASTools/Program.cs
+++ src/CASTools/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Windows.Forms;
 
 namespace XMODS
@@ -13,8 +16,14 @@ namespace XMODS
         [STAThread]
         static void Main()
         {
+            Debug.WriteLine("Starting app");
+            #if NETCORE
+            // Application.SetDefaultFont(new Font(new FontFamily("Microsoft Sans Serif"), 12f));
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Application.SetHighDpiMode(HighDpiMode.DpiUnaware);
+            #endif
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetCompatibleTextRenderingDefault(true);
             Application.Run(new Form1());
         }
     }
